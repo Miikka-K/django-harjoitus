ffb3115f73f03cd9de3def87426eb6217d9c4abf
Miikka-K <51471@edu.raseko.fi>
2024-02-05 13:10:49 +0200

    Päivitä näytä näkymää

--- kysely/views.py
+++ kysely/views.py
@@ -1,6 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+
+from .models import Kysymys
 
 
 def indeksi(request):
-    return HttpResponse("Heippa! Olet kysely-appin index-sivulla")
+    kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
+    context = {
+        "kysymykset": kysymyslista,
+    }
+    return render(request, "kysely/indeksi.html", context)
+
+
+def näytä(request, kysymys_id):
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    return render(request, "kysely/näytä.html", {"kysymys": kysym})
+
+
+def tulokset(request, question_id):
+    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia.")
+
+
+def äänestä(request, question_id):
+    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
