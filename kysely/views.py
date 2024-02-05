395abf6af87c02a6d47b1d96f158caca9d25f52e
Miikka-K <51471@edu.raseko.fi>
2024-02-05 15:58:19 +0200

    Jottai

--- kysely/views.py
+++ kysely/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-from .models import Kysymys
+from .models import Kysymys, Vaihtoehto
 
 
 def indeksi(request):
@@ -17,9 +18,31 @@ def näytä(request, kysymys_id):
     return render(request, "kysely/näytä.html", {"kysymys": kysym})
 
 
-def tulokset(request, question_id):
-    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia.")
+def tulokset(request, kysymys_id):
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    return render(request, "kysely/tulokset.html", {"kysymys": kysym})
 
 
-def äänestä(request, question_id):
-    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
+def äänestä(request, kysymys_id): 
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    try:
+        valittu = kysym.vaihtoehto_set.get(pk=request.POST["valittu"])
+    except (KeyError, Vaihtoehto.DoesNotExist):
+        # näytä kysymyslomake uudelleen
+        return render(
+            request,
+            "kysely/näytä.html",
+            {
+                "kysymys": kysym,
+                "virheviesti": "Et valinnut mitään vaihtoehtoa.",
+            },
+        )
+    else:
+        valittu.ääniä += 1
+        valittu.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        osoite = reverse("kysely:tulokset", args=(kysym.id,))
+        return HttpResponseRedirect(osoite)
+    
