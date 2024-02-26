99dd2997a09e62fb540b8532035233240f95edb8
Miikka-K <51471@edu.raseko.fi>
2024-02-26 13:11:08 +0200

    Admin: laita kysymys autocomplete-kentäksi

--- kysely/admin.py
+++ kysely/admin.py
@@ -16,9 +16,11 @@ class KysymysAdmin(admin.ModelAdmin):
     ]
     inlines = [VastausvaihtoehtoInline]
     list_display = ["teksti", "julkaisupvm", "onko_julkaistu_lähiaikoina"]
+    search_fields = ["teksti"]
 
 
 @admin.register(Vaihtoehto)
 class VaihtoehtoAdmin(admin.ModelAdmin):
     list_display = ["kysymys", "teksti"]
     search_fields = ["teksti", "kysymys__teksti"]
+    autocomplete_fields = ["kysymys"]
